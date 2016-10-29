57c8f4a2febdc251c35d94dc774c14f6e72f4b1b
MashaLah <lahoska91@mail.ru>
2016-10-29 23:33:50 +0300

    Add Task1.

---
+++ Task1/EuclideanAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class EuclideanAlgorithm
+    {
+        /// <summary>
+        /// Finds the greatest common divisor (GCD) of some numbers.
+        /// </summary>
+        public static int FindGCD(int [] array)
+        public static int FindGCD(params int[] array)
+        {
+            while (array[1] != 0)
+            {
+                array[1] = array[0] % (array[0] = array[1]);
+            }
+            int result = array[0];
+            for (int i = 2; i < array.Length; i++)
+            {
+                while (array[i] != 0)
+                {
+                    array[i] = result % (result = array[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Task1Tests/EuclideanAlgorithmTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+
+namespace Task1Tests
+{
+    [TestFixture]
+    public class EuclideanAlgorithmTests
+    {
+        /// <summary>
+        /// A test for FindGCD method.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="expected"></param>
+        [TestCase(9,18, 27, 9, 81)]
+        public void FindGCD_18and27and9and81_9Returned(int expected, params int [] array)
+        {
+            //act
+            int actual = EuclideanAlgorithm.FindGCD(array);
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
