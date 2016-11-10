860eecc8ebd382b4f2ac5b20062a99600ef8f532
MashaLah <lahoska91@mail.ru>
2016-11-10 15:53:34 +0300

    Add Adapter

---
+++ Task3/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Adapter:IComparer<int[]>
+    {
+        Func<int[],int[],int> sortingFunction;
+
+        public Adapter(Func<int[], int[], int> sortingFunction)
+        {
+            this.sortingFunction = sortingFunction;
+        }
+
+        public int Compare(int[] firstArray, int[] secondArray)
+        {
+            return sortingFunction(firstArray,secondArray);
+        }
+    }
+}
